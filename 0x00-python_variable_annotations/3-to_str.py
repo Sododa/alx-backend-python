f080f360de3955e89cfc4911a7b3c94ed1898bd7
Sododa <sophieododa@gmail.com>
2024-08-01 12:39:51 +0300

    variables

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""task 3"""
+
+
 def to_str(n: float) -> str:
+    """return string"""
     return str(n)
--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""task 4"""
 a: int = 1
 pi: float = 3.14
 school: str = 'Holberton'
