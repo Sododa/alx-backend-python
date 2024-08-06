10f1829d14652c9dc9f21f707b4be58240092fd7
Sododa <sophieododa@gmail.com>
2024-08-06 13:10:22 +0300

    0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ import random
 
 async def async_generator():
     """Write a coroutine called async_generator that takes no arguments."""
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
